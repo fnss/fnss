9c047a9c3b6fe6fc3126fc99031d0ccb514fd8c3
Lorenzo Saino <l.saino@ucl.ac.uk>
2013-04-11 16:55:44 +0100

    Improve core examples
    This commit improves the examples of the core component, by making
    the difference between networkx and fnss functions clearer.
    
    This commit also extends the rocketfuel_tm example to show how to
    generate a traffic matrix with specific origins and destinations
    randomly picked among topology nodes.

--- core/examples/datacenter.py
+++ core/examples/datacenter.py
@@ -5,32 +5,32 @@ Datacenter topology
 This example shows how to generate a datacenter topology, assign capacities
 and save everything to an XML file
 """
-from fnss import *
-from networkx import get_edge_attributes
+import fnss
+import networkx as nx
 
 # create a topology with 10 core switches, 20 edge switches and 10 servers
 # per switch (i.e. 200 servers in total)
-topology = two_tier_topology(n_core=10, n_edge=20, n_servers=10)
+topology = fnss.two_tier_topology(n_core=10, n_edge=20, n_servers=10)
 
 # assign capcities. Assign let's set links connecting servers to edge switches
 # to 1 Gbps and links connecting core and edge switches to 10 Gbps.
 
 # get list of core_edge links and edge_leaf links
-link_types = get_edge_attributes(topology, 'type')
+link_types = nx.get_edge_attributes(topology, 'type')
 core_edge_links = [link for link in link_types
                    if link_types[link] == 'core_edge']
 edge_leaf_links = [link for link in link_types
                    if link_types[link] == 'edge_leaf']
 
 # assign capacities
-set_capacities_constant(topology, 1, 'Gbps', edge_leaf_links)
-set_capacities_constant(topology, 10, 'Gbps', core_edge_links)
+fnss.set_capacities_constant(topology, 1, 'Gbps', edge_leaf_links)
+fnss.set_capacities_constant(topology, 10, 'Gbps', core_edge_links)
 
 # assign weight 1 to all links
-set_weights_constant(topology, 1)
+fnss.set_weights_constant(topology, 1)
 
 # assign delay of 10 nanoseconds to each link
-set_delays_constant(topology, 10, 'ns')
+fnss.set_delays_constant(topology, 10, 'ns')
 
 # save topology to a file
-write_topology(topology, 'datacenter_topology.xml')
+fnss.write_topology(topology, 'datacenter_topology.xml')
