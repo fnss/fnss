1b04e563418fcdd7444a58f5f63d71c2bc3df71a
Lorenzo Saino <l.saino@ucl.ac.uk>
2014-03-27 14:56:12 +0000

    Add script for automatic install on Ubuntu
    This commit adds a script that allows users to install the core
    library and all its dependencies with a single command on Ubuntu
    machines.

---
+++ core/ubuntu_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# This script automatically download and install the FNSS core library along with all its dependencies
+#
+# This script has been tested only on Ubuntu version 12.04+
+#
+
+# Makes sure all packages installed are up-to-date
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install all dependencies available on Ubuntu package repository
+sudo apt-get install python python-pip python-setuptools python-numpy python-networkx python-mako python-nose
+
+# Install other optional dependencies not available on Ubuntu package repository
+sudo pip install sphinx numpydoc
+
+# Install FNSS core library
+sudo pip install fnss
