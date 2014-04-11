fbf80f9bfd754cc2ffb2dcfc7c28d6935b193399
Lorenzo Saino <l.saino@ucl.ac.uk>
2014-04-11 21:33:55 +0100

    Add test cases for randmodel topologies with user-provided seed
    This commit adds test cases to validate that all random topology
    generators behave correctly when seeds are provided. These test cases
    have been added to prevent the reappearance of the issue corrected
    by #11.

--- core/test/test/test_topologies/test_randmodels.py
+++ core/test/test/test_topologies/test_randmodels.py
@@ -7,6 +7,7 @@ else:
     except ImportError:
         raise ImportError("The unittest2 package is needed to run the tests.") 
 del sys
+import random
 import networkx as nx
 import fnss
 
@@ -30,6 +31,30 @@ class Test(unittest.TestCase):
         self.assertRaises(ValueError, fnss.erdos_renyi_topology, 40, 1.2)
         self.assertRaises(ValueError, fnss.erdos_renyi_topology, 50, -0.2)
     
+    def test_erdos_renyi_topology_no_seed(self):
+        a = fnss.erdos_renyi_topology(100, 0.2)
+        b = fnss.erdos_renyi_topology(100, 0.2)
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+
+    def test_erdos_renyi_topology_constant_seed(self):
+        a = fnss.erdos_renyi_topology(100, 0.2, seed=1)
+        b = fnss.erdos_renyi_topology(100, 0.2, seed=2)
+        c = fnss.erdos_renyi_topology(100, 0.2, seed=1)
+        self.assertEqual(set(a.edges()), set(c.edges()))
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+
+    def test_fast_erdos_renyi_topology_no_seed(self):
+        a = fnss.erdos_renyi_topology(100, 0.2, fast=True)
+        b = fnss.erdos_renyi_topology(100, 0.2, fast=True)
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+
+    def test_fast_erdos_renyi_topology_constant_seed(self):
+        a = fnss.erdos_renyi_topology(100, 0.2, fast=True, seed=1)
+        b = fnss.erdos_renyi_topology(100, 0.2, fast=True, seed=2)
+        c = fnss.erdos_renyi_topology(100, 0.2, fast=True, seed=1)
+        self.assertEqual(set(a.edges()), set(c.edges()))
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+
     def test_waxman_1_topology(self):
         self.assertRaises(ValueError, fnss.waxman_1_topology, 0, 0.5, 0.5, 10)
         self.assertRaises(ValueError, fnss.waxman_1_topology, 10, 0, 0.5, 10)
@@ -40,7 +65,19 @@ class Test(unittest.TestCase):
         length = nx.get_edge_attributes(topology, 'length')
         self.assertGreaterEqual(20, max(length.values()))
         self.assertLessEqual(0, min(length.values()))
-        
+    
+    def test_waxman_1_topology_no_seed(self):  
+        a = fnss.waxman_1_topology(100, alpha=0.5, beta=0.6, L=20)
+        b = fnss.waxman_1_topology(100, alpha=0.5, beta=0.6, L=20)
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+
+    def test_waxman_1_topology_constant_seed(self):  
+        a = fnss.waxman_1_topology(100, alpha=0.5, beta=0.6, L=20, seed=1)
+        b = fnss.waxman_1_topology(100, alpha=0.5, beta=0.6, L=20, seed=2)
+        c = fnss.waxman_1_topology(100, alpha=0.5, beta=0.6, L=20, seed=1)
+        self.assertEqual(set(a.edges()), set(c.edges()))
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+    
     def test_waxman_2_topology(self):
         self.assertRaises(ValueError, fnss.waxman_2_topology, 0, 0.5, 0.6)
         self.assertRaises(ValueError, fnss.waxman_2_topology, 10, 0, 0.5)
@@ -64,7 +101,19 @@ class Test(unittest.TestCase):
         self.assertLessEqual(-2, min(y.values()))
         self.assertGreaterEqual(1, max(x.values()))
         self.assertGreaterEqual(2, max(y.values()))
-        
+
+    def test_waxman_2_topology_no_seed(self):  
+        a = fnss.waxman_2_topology(100, alpha=0.5, beta=0.6, domain=(-1, -2, 1, 2))
+        b = fnss.waxman_2_topology(100, alpha=0.5, beta=0.6, domain=(-1, -2, 1, 2))
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+
+    def test_waxman_2_topology_constant_seed(self):  
+        a = fnss.waxman_2_topology(100, alpha=0.5, beta=0.6, domain=(-1, -2, 1, 2), seed=1)
+        b = fnss.waxman_2_topology(100, alpha=0.5, beta=0.6, domain=(-1, -2, 1, 2), seed=2)
+        c = fnss.waxman_2_topology(100, alpha=0.5, beta=0.6, domain=(-1, -2, 1, 2), seed=1)
+        self.assertEqual(set(a.edges()), set(c.edges()))
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+
     def test_barabasi_albert_topology(self):
         self.assertRaises(ValueError, fnss.barabasi_albert_topology, 0, 20, 30)
         self.assertRaises(ValueError, fnss.barabasi_albert_topology, 50, 40, 20)
@@ -74,6 +123,18 @@ class Test(unittest.TestCase):
         topology = fnss.barabasi_albert_topology(100, 11, 16)
         self.assertEqual(100, topology.number_of_nodes())
         
+    def test_barabasi_albert_topology_no_seed(self):
+        a = fnss.barabasi_albert_topology(100, 11, 16)
+        b = fnss.barabasi_albert_topology(100, 11, 16)
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+
+    def test_barabasi_albert_topology_constant_seed(self):
+        a = fnss.barabasi_albert_topology(100, 11, 16, seed=1)
+        b = fnss.barabasi_albert_topology(100, 11, 16, seed=2)
+        c = fnss.barabasi_albert_topology(100, 11, 16, seed=1)
+        self.assertEqual(set(a.edges()), set(c.edges()))
+        self.assertNotEqual(set(a.edges()), set(b.edges()))  
+
     def test_extended_barabasi_albert_topology(self):
         self.assertRaises(ValueError, fnss.extended_barabasi_albert_topology, 0, 20, 30, 0.2, 0.3)
         self.assertRaises(ValueError, fnss.extended_barabasi_albert_topology, 60, 30, 20, 0.2, 0.3)
@@ -86,6 +147,18 @@ class Test(unittest.TestCase):
         topology = fnss.extended_barabasi_albert_topology(100, 11, 16, 0.2, 0.3)
         self.assertEqual(100, topology.number_of_nodes())
     
+    def test_extended_barabasi_albert_topology_no_seed(self):
+        a = fnss.extended_barabasi_albert_topology(100, 11, 16, 0.2, 0.3)
+        b = fnss.extended_barabasi_albert_topology(100, 11, 16, 0.2, 0.3)
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+
+    def test_extended_barabasi_albert_topology_constant_seed(self):
+        a = fnss.extended_barabasi_albert_topology(100, 11, 16, 0.2, 0.3, seed=1)
+        b = fnss.extended_barabasi_albert_topology(100, 11, 16, 0.2, 0.3, seed=2)
+        c = fnss.extended_barabasi_albert_topology(100, 11, 16, 0.2, 0.3, seed=1)
+        self.assertEqual(set(a.edges()), set(c.edges()))
+        self.assertNotEqual(set(a.edges()), set(b.edges()))    
+
     def test_glp_topology(self):
         self.assertRaises(ValueError, fnss.glp_topology, 0, 20, 30, 0.4, -0.5)
         self.assertRaises(ValueError, fnss.glp_topology, 100, 30, 20, 0.4, -0.5)
@@ -95,3 +168,15 @@ class Test(unittest.TestCase):
         self.assertRaises(ValueError, fnss.glp_topology, 20, 11, 16, 1.5, 0.5)
         topology = fnss.glp_topology(20, 11, 16, 0.5, 0.5)
         self.assertEqual(20, topology.number_of_nodes())
+               
+    def test_glp_topology_no_seed(self):
+        a = fnss.glp_topology(100, 15, 20, 0.5, 0.5)
+        b = fnss.glp_topology(100, 15, 20, 0.5, 0.5)
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
+    
+    def test_glp_topology_constant_seed(self):
+        a = fnss.glp_topology(100, 15, 20, 0.5, 0.5, seed=1)
+        b = fnss.glp_topology(100, 15, 20, 0.5, 0.5, seed=2)
+        c = fnss.glp_topology(100, 15, 20, 0.5, 0.5, seed=1)
+        self.assertEqual(set(a.edges()), set(c.edges()))
+        self.assertNotEqual(set(a.edges()), set(b.edges()))
