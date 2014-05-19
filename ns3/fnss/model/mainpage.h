d1d44edc204fd752faa203ca01fb7da17de95230
Lorenzo Saino <l.saino@ucl.ac.uk>
2014-05-19 13:22:35 +0100

    Add Doxygen's mainpage.h to ns-3 library
    This commit adds the mainpage.h file to ns-3 library. This
    file contains the text that is shown in the homepage of
    Doxygen built documentation.

---
+++ ns3/fnss/model/mainpage.h
@@ -0,0 +1,16 @@
+/*
+ * This page contains the text to be put on the Doxygen main page
+ */
+
+/**
+ * @mainpage  ns-3 library documentation
+ *
+ * This is the ns-3 library documentation of the Fast Network Simulation Setup
+ * (FNSS) toolchain.
+ *
+ * This library allows users to import topologies or event schedules generated
+ * by the FNSS core library into the ns-3 simulator.
+ *
+ * For further information, please refer to the README file or visit the
+ * website <a href="http://fnss.github.com">http://fnss.github.io</a>
+ */
