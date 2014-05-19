81527134922c105c701237da5bd6939031411664
Lorenzo Saino <l.saino@ucl.ac.uk>
2014-05-19 17:16:22 +0100

    Make ubuntu_install.sh script set up Python3 on Ubuntu 14.04
    Extend the ubuntu_install.sh script such that, if run on Ubuntu
    14.04, it installs all dependencies for running FNSS also on
    Python 3, while on versions 12.xx and 12.xx set things up only
    for Python 2.7

--- core/ubuntu_install.sh
+++ core/ubuntu_install.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 #
-# This script automatically downloads and installs the FNSS core library along with all its dependencies
+# This script automatically downloads and installs the FNSS core
+# library along with all its dependencies to be used with
+# Python 2.7
 #
 # This script has been tested only on Ubuntu version 12.04+
 #
+# If this script is run on Ubuntu 14.04, it also install all dependencies
+# for running FNSS with Python 3.
 
 # Makes sure all packages installed are up-to-date
 sudo apt-get update
 sudo apt-get upgrade
 
 # Install all dependencies available on Ubuntu package repository
-sudo apt-get install python python-pip python-setuptools python-numpy python-networkx python-mako python-nose
+sudo apt-get install python python-pip python-setuptools python-numpy python-scipy python-networkx python-mako python-nose
 
 # Install other optional dependencies not available on Ubuntu package repository
 sudo pip install sphinx numpydoc
 
 # Install FNSS core library
-sudo pip install fnss
+sudo pip install -U fnss
+
+# Install FNSS and all dependencies for Python 3 (only on Ubuntu 14.04)
+# Note: NetworkX must be installed via pip3 instead of Aptitude because of a
+# bug that crashes the installation of python3-network apt package if
+# python-networkx is installed
+if type "lsb_release" > /dev/null; then 
+	if [type "lsb_release" > /dev/null -a `lsb_release -r | awk '{print $2}'` = "14.04"]; then
+		sudo apt-get install python3-pip python3-setuptools python3-numpy python3-scipy python3-mako python3-nose
+		sudo pip3 install -U sphinx numpydoc networkx fnss
+	fi
+fi
