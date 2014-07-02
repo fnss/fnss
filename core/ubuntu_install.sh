51766da02e3e6dee06697572b64a58b06f12b618
Lorenzo Saino <l.saino@ucl.ac.uk>
2014-07-02 10:41:38 +0200

    Comment out Python 3 installation in Ubuntu install script
    This change is suggested to minimize the risk of issues arising
    during the execution of the script

--- core/ubuntu_install.sh
+++ core/ubuntu_install.sh
@@ -6,34 +6,38 @@
 #
 # This script has been tested only on Ubuntu version 12.04+
 #
-# If this script is run on Ubuntu 14.04, it also install all dependencies
-# for running FNSS with Python 3.
+# If this script is run on Ubuntu 14.04, it may be possible to
+# also install all dependencies for running FNSS with Python 3.
+# The code doing this is commented out by default. 
 
 # Makes sure all packages installed are up-to-date
 sudo apt-get update
 sudo apt-get upgrade
 
 # Install all dependencies available on Ubuntu package repository
-sudo apt-get install python python-pip python-setuptools python-numpy python-scipy python-networkx python-mako python-nose
+sudo apt-get -y -q install python python-pip python-setuptools python-numpy python-scipy python-networkx python-mako python-nose
 
 # Install other optional dependencies not available on Ubuntu package repository
-sudo pip install sphinx numpydoc
+sudo pip install -U sphinx numpydoc
 
 # Install FNSS core library
 sudo pip install -U fnss
 
 # Install C++ and Java dependencies
-sudo apt-get install clang doxygen ant
+sudo apt-get -y -q install clang doxygen ant
 
 # Install FNSS and all dependencies for Python 3 (only on Ubuntu 14.04)
 # Note: NetworkX must be installed via pip3 instead of Aptitude because of a
 # bug that crashes the installation of python3-network apt package if
 # python-networkx is installed
-VERSION=$(lsb_release -r 2>/dev/null | awk '{print $2}')
-type "lsb_release" > /dev/null
-if [ $? -eq 0 ]; then
-	if [ $VERSION = "14.04" ]; then
-		sudo apt-get install python3-pip python3-setuptools python3-numpy python3-scipy python3-mako python3-nose
-		sudo pip3 install -U sphinx numpydoc networkx fnss
-	fi
-fi
+#
+# Uncomment the following code to install Python 3 dependencies
+#
+# VERSION=$(lsb_release -r 2>/dev/null | awk '{print $2}')
+# type "lsb_release" > /dev/null
+# if [ $? -eq 0 ]; then
+# 	if [ $VERSION = "14.04" ]; then
+# 		sudo apt-get -y -q install python3-pip python3-setuptools python3-numpy python3-scipy python3-mako python3-nose
+# 		sudo pip3 install -U sphinx numpydoc networkx fnss
+# 	fi
+# fi
