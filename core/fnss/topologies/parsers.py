c0cb25eb09948c5c4f32c1bcfc3b40c329e7c96a
Lorenzo Saino <l.saino@ucl.ac.uk>
2015-06-03 16:33:43 +0300

    Update summary table of RocketFuel latency topologies

--- core/fnss/topologies/parsers.py
+++ core/fnss/topologies/parsers.py
@@ -150,10 +150,10 @@ def parse_rocketfuel_isp_latency(latencies_path, weights_path=None):
     +======+=====================+=======+========+=======+===================+
     | 1221 | Telstra (Australia) | world | AUS    |  108  |        104        |
     | 1239 | Sprintlink (US)     | world | US     |  315  |        315        |
-    | 1755 | EBONE (Europe)      | world | Europe |  161  |        161        |
-    | 3257 | Tiscali (Europe)    | world | Europe |   87  |         87        |
-    | 3967 | Exodus (US)         | world | US     |  141  |        138        |
-    | 6461 | Abovenet (US)       | world | US     |  79   |         79        |
+    | 1755 | EBONE (Europe)      | world | Europe |   87  |         87        |
+    | 3257 | Tiscali (Europe)    | world | Europe |  161  |        161        |
+    | 3967 | Exodus (US)         | world | US     |   79  |         79        |
+    | 6461 | Abovenet (US)       | world | US     |  141  |        138        |
     +------+---------------------+-------+--------+-------+-------------------+
 
     Parameters
