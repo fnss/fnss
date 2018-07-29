0f270a159cf49aaea8a65671a7debaea4a0d97e8
Lorenzo Saino <l.saino@ucl.ac.uk>
2018-07-29 14:49:28 +0100

    Bump version to 0.8.1

--- fnss/release.py
+++ fnss/release.py
@@ -10,7 +10,7 @@ author_email = 'fnss.dev@gmail.com'
 
 # version information
 # Used by __init__, doc and setup
-version = '0.8.0'
+version = '0.8.1'
 
 # License information
 # Used by __init__, doc and setup
