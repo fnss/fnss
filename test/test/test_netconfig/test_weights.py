0d906d55978b4f73d35bb0aa181c57a39c9ed47a
marci543 <marci543@users.noreply.github.com>
2019-08-08 17:47:20 +0200

    Add multigraph tests to test_weights.py

--- test/test/test_netconfig/test_weights.py
+++ test/test/test_netconfig/test_weights.py
@@ -2,13 +2,21 @@ import networkx as nx
 import pytest
 
 import fnss
+from test_topologies.test_topology import has_parallel_edges
+# required import for pytest fixture
+# noinspection PyUnresolvedReferences
+from test_topologies.test_topology import use_multigraph, topology_converter
 
 
-# TODO add multigraph tests
 @pytest.fixture
-def topology_info():
+def topology_info(topology_converter, use_multigraph):
     # set up topology used for all traffic matrix tests
-    topo = fnss.k_ary_tree_topology(3, 4)
+    topo = topology_converter(fnss.k_ary_tree_topology(3, 4))
+
+    # duplicate an edge, no effect for simple graphs
+    topo.add_edge(*next(iter(topo.edges.keys()))[:2])
+    assert has_parallel_edges(topo) == use_multigraph
+
     capacities = [10, 20]
     odd_links = [link for link in topo.edges
                  if sum(link) % 2 == 1]
@@ -44,10 +52,16 @@ def test_weights_delays(topology_info):
                for data_dict in topo.edges.values())
 
 
-def test_clear_weights():
+def test_clear_weights(topology_converter, use_multigraph):
     # create new topology to avoid parameters pollution
-    G = fnss.star_topology(12)
-    fnss.set_weights_constant(G, 3, None)
-    assert G.number_of_edges() == len(nx.get_edge_attributes(G, 'weight'))
-    fnss.clear_weights(G)
-    assert 0 == len(nx.get_edge_attributes(G, 'weight'))
+    topo = topology_converter(fnss.star_topology(12))
+
+    # duplicate an edge, no effect for simple graphs
+    topo.add_edge(*next(iter(topo.edges.keys()))[:2])
+    assert has_parallel_edges(topo) == use_multigraph
+
+    fnss.set_weights_constant(topo, 3, None)
+    assert topo.number_of_edges() == len(nx.get_edge_attributes(topo, 'weight'))
+
+    fnss.clear_weights(topo)
+    assert 0 == len(nx.get_edge_attributes(topo, 'weight'))
