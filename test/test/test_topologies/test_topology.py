97530292d6e70229f80db1c8f7a4700e8ed4af77
marci543 <marci543@users.noreply.github.com>
2019-08-08 15:47:13 +0200

    Add multigraph tests to test_topology.py

--- test/test/test_topologies/test_topology.py
+++ test/test/test_topologies/test_topology.py
@@ -7,11 +7,62 @@ import fnss
 TMP_DIR = environ['test.tmp.dir'] if 'test.tmp.dir' in environ else None
 
 
-# TODO add multigraph tests
+def has_parallel_edges(graph):
+    return graph.is_multigraph() and \
+           any(len(key_dict) > 1
+               for u, v_dict in graph.adj.items()
+               for v, key_dict in v_dict.items())
+
+
+@pytest.fixture(params=[True, False], ids=['multigraph', 'simple graph'])
+def use_multigraph(request):
+    return request.param
+
+
 @pytest.fixture
-def topology():
+def topology_converter(use_multigraph):
+    def to_multigraph(topology):
+        multigraph = topology.to_multigraph()
+        assert multigraph.is_multigraph()
+        return multigraph
+
+    def identity(topology):
+        assert not topology.is_multigraph()
+        return topology
+
+    return to_multigraph if use_multigraph \
+        else identity
+
+
+@pytest.fixture
+def topology(topology_converter):
+    return topology_converter(fnss.Topology())
+
+
+@pytest.fixture
+def dir_topology(topology_converter):
+    return topology_converter(fnss.DirectedTopology())
+
+
+@pytest.fixture
+def ring_topology(topology_converter):
+    return topology_converter(fnss.ring_topology(3))
+
+
+@pytest.fixture
+def star_topology(topology_converter):
+    return topology_converter(fnss.star_topology(3))
+
+
+@pytest.fixture
+def glp_topology(topology_converter, use_multigraph):
     # set up topology used for all traffic matrix tests
-    topology = fnss.glp_topology(n=50, m=1, m0=10, p=0.2, beta=-2, seed=1)
+    topology = topology_converter(fnss.glp_topology(n=50, m=1, m0=10, p=0.2, beta=-2, seed=1))
+
+    # duplicate an edge, no effect for simple graphs
+    topology.add_edge(*next(iter(topology.edges.keys()))[:2])
+    assert has_parallel_edges(topology) == use_multigraph
+
     fnss.set_capacities_random(topology, {10: 0.5, 20: 0.3, 40: 0.2},
                                capacity_unit='Mbps')
     fnss.set_delays_constant(topology, 2, delay_unit='ms')
@@ -27,13 +78,17 @@ def topology():
     return topology
 
 
-def test_base_topology_class():
+def test_base_topology_class(topology, use_multigraph):
     weight = 2
     capacity = 3
     delay = 4
     buffer_size = 5
-    topology = fnss.Topology()
     topology.add_path([1, 2, 3])
+
+    # no effect for simple graphs
+    topology.add_edge(1, 2)
+    assert has_parallel_edges(topology) == use_multigraph
+
     fnss.set_weights_constant(topology, weight)
     fnss.set_capacities_constant(topology, capacity)
     fnss.set_delays_constant(topology, delay)
@@ -49,13 +104,12 @@ def test_base_topology_class():
         assert buffer_size == buffer_sizes[e]
 
 
-def test_topology_class():
-    topology = fnss.Topology()
+def test_topology_class(topology, use_multigraph):
     topology.add_edge(1, 2)
     assert 1 == topology.number_of_edges()
     # add parallel link if it is multigraph
     topology.add_edge(2, 1)
-    if topology.is_multigraph():
+    if use_multigraph:
         assert 2 == topology.number_of_edges()
     else:
         assert 1 == topology.number_of_edges()
@@ -65,21 +119,19 @@ def test_topology_class():
     topology.add_edge('2', '1')
 
     assert 4 == topology.number_of_nodes()
-    if topology.is_multigraph():
+    if use_multigraph:
         assert 4 == topology.number_of_edges()
     else:
         assert 2 == topology.number_of_edges()
 
 
-def test_directed_topology_class():
-    topology = fnss.DirectedTopology()
-    topology.add_edge(1, 2)
-    topology.add_edge(2, 1)
-    assert 2 == topology.number_of_edges()
+def test_directed_topology_class(dir_topology):
+    dir_topology.add_edge(1, 2)
+    dir_topology.add_edge(2, 1)
+    assert 2 == dir_topology.number_of_edges()
 
 
-def test_od_pairs_from_topology_directed():
-    dir_topology = fnss.DirectedTopology()
+def test_od_pairs_from_topology_directed(dir_topology):
     dir_topology.add_edge(0, 1)
     dir_topology.add_edge(1, 0)
     dir_topology.add_edge(1, 2)
@@ -92,8 +144,8 @@ def test_od_pairs_from_topology_directed():
         assert od in od_pairs
 
 
-def test_od_pairs_from_topology_undirected():
-    topology = fnss.ring_topology(3)
+def test_od_pairs_from_topology_undirected(ring_topology):
+    topology = ring_topology
     topology.add_path([7, 8, 9])  # isolated node: no flows from/to this node
     od_pairs = fnss.od_pairs_from_topology(topology)
     expected_od_pairs = [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1),
@@ -103,8 +155,7 @@ def test_od_pairs_from_topology_undirected():
         assert od in od_pairs
 
 
-def test_fan_in_out_capacities_directed():
-    dir_topology = fnss.DirectedTopology()
+def test_fan_in_out_capacities_directed(dir_topology):
     dir_topology.add_edge(0, 1)
     dir_topology.add_edge(1, 0)
     dir_topology.add_edge(1, 2)
@@ -115,8 +166,8 @@ def test_fan_in_out_capacities_directed():
     assert {0: 10, 1: 20, 2: 0, 3: 10} == out_cap
 
 
-def test_fan_in_out_capacities_undirected():
-    topology = fnss.star_topology(3)
+def test_fan_in_out_capacities_undirected(star_topology):
+    topology = star_topology
     fnss.set_capacities_constant(topology, 10, 'Mbps')
     in_cap, out_cap = fnss.fan_in_out_capacities(topology)
     assert {0: 30, 1: 10, 2: 10, 3: 10} == in_cap
@@ -124,13 +175,20 @@ def test_fan_in_out_capacities_undirected():
 
 
 @pytest.mark.skipif(TMP_DIR is None, reason="Temp folder not present")
-def test_read_write_topology(topology):
+def test_read_write_topology(glp_topology, use_multigraph):
+    topology = glp_topology
     tmp_topo_file = path.join(TMP_DIR, 'toporw.xml')
     fnss.write_topology(topology, tmp_topo_file)
     assert path.exists(tmp_topo_file)
-    read_topo = fnss.read_topology(tmp_topo_file)
+    read_topo = fnss.read_topology(tmp_topo_file, use_multigraph)
+
+    assert read_topo.is_directed() == topology.is_directed()
+    assert read_topo.is_multigraph() == use_multigraph
+    assert has_parallel_edges(read_topo) == use_multigraph
+
     assert len(topology) == len(read_topo)
     assert topology.number_of_edges() == read_topo.number_of_edges()
+
     assert 'tcp' == fnss.get_stack(read_topo, 2)[0]
     assert 1024 == fnss.get_stack(read_topo, 2)[1]['rcvwnd']
     assert 'cubic' == fnss.get_stack(read_topo, 2)[1]['protocol']
